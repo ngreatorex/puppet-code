f65f0d9266144f9069929fb3a679d055cdc86784
Neil Greatorex <neil@greatorex.me.uk>
2018-02-16 15:52:15 +0000

    Latest code

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -4,6 +4,9 @@ node 'raspi-monitoring.lan' {
 
 node 'raspi-puppet.win.lan' {
 	include local::linux
+	class { 'puppetdb::master::config':
+		puppetdb_server => 'ubuntu.lan'
+	}
 }
 
 node 'raspi-c.lan' {
@@ -16,6 +19,34 @@ node 'raspi-weather.lan' {
 
 node 'raspi-e.win.lan' {
 
+}
+
+node 'ubuntu.lan' {
+	include local::linux
+	include puppetdb
+	class { 'puppetdb::master::config':
+		puppet_confdir => '/etc/puppetlabs/puppet',
+		puppet_conf => '/etc/puppetlabs/puppet/puppet.conf',
+	}
+
+	class {'::puppetexplorer':
+		vhost_options => {
+			rewrites  => [ { 
+				rewrite_rule => ['^/api/metrics/v1/mbeans/puppetlabs.puppetdb.query.population:type=default,name=(.*)$  https://%{HTTP_HOST}/api/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=$1 [R=301,L]'] 
+			} ]
+		}
+	}
+
+	class { 'apache::mod::wsgi': }
+	class { 'puppetboard':
+		manage_git        => true,
+		manage_virtualenv => true,
+		enable_catalog    => true,
+	}
+	class { 'puppetboard::apache::vhost':
+		vhost_name => 'puppetboard.lan',
+		port       => 80,
+	}
 }
 
 node default {
--- environments/production/modules/local/manifests/linux.pp
+++ environments/production/modules/local/manifests/linux.pp
@@ -1,8 +1,13 @@
 class local::linux {
 	include local::putty_curses
+	include ::profiled
+
+	profiled::script { 'vim.sh':
+		content => 'alias vim=vim.tiny',
+	}
 
 	class { 'ntp':
-	  servers => [ 'ntp.lan' ],
+		servers => [ 'ntp.lan' ],
 	}
 }
 
