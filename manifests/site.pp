b1aa22dad6403dffc40d78de41c1f915dac5d919
Neil Greatorex <neil@greatorex.me.uk>
2019-10-10 19:08:16 +0100

    Comment out puppetexplorer for now

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,13 +30,13 @@ node 'ubuntu.lan' {
 		puppet_conf => '/etc/puppetlabs/puppet/puppet.conf',
 	}
 
-	class {'::puppetexplorer':
-		vhost_options => {
-			rewrites  => [ { 
-				rewrite_rule => ['^/api/metrics/v1/mbeans/puppetlabs.puppetdb.query.population:type=default,name=(.*)$  https://%{HTTP_HOST}/api/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=$1 [R=301,L]'] 
-			} ]
-		}
-	}
+#	class {'::puppetexplorer':
+#		vhost_options => {
+#			rewrites  => [ { 
+#				rewrite_rule => ['^/api/metrics/v1/mbeans/puppetlabs.puppetdb.query.population:type=default,name=(.*)$  https://%{HTTP_HOST}/api/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=$1 [R=301,L]'] 
+#			} ]
+#		}
+#	}
 
 	class { 'apache::mod::wsgi': }
 	class { 'puppetboard':
