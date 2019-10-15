8adcf2667a7a85e3c69e8aa4eaa5eedb63515dbc
Neil Greatorex <neil@greatorex.me.uk>
2019-10-15 17:18:20 +0100

    Update NTP to allow Icinga host to query itself by hostname

--- site-modules/local/manifests/ntpd.pp
+++ site-modules/local/manifests/ntpd.pp
@@ -2,6 +2,13 @@ class local::ntpd {
 	if $trusted['hostname'] != "raspi-e" {
 		class { 'ntp':
 			servers => [ 'ntp.lan' ],
+			restrict  => [
+				'default nomodify notrap nopeer noquery',
+				'-6 default nomodify notrap nopeer noquery'
+				'127.0.0.1',
+				'127.0.1.1',
+				'-6 ::1',
+			],
 		}
 	}
 }
